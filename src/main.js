71512a608d4f66e2f6409bb6121925c1c4700d1b
Smilyan Pavlov <smilyan.pavlov@iris.co.uk>
2021-12-23 08:31:38 +0000

    Re-organising file structure

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import Login from './features/auth/login/Login.vue'
-import ProjectsList from './features/projects/projects-list/ProjectsList.vue'
-import AdminDashboard from './features/admin/dashboard/AdminDashboard.vue'
+import Login from './features/auth/components/Login.vue'
+import ProjectsList from './features/projects/components/ProjectsList.vue'
+import AdminDashboard from './features/admin/components/AdminDashboard.vue'
 
 const routes = [
   { path: '/', component: ProjectsList },
